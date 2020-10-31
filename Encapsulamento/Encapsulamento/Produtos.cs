028694ab2fc273ddee7571d4f0c8b31b694e423c
JulliannARamos <69683252+JulliannARamos@users.noreply.github.com>
2020-10-30 22:30:36 -0300

    Add files via upload

---
+++ Encapsulamento/Encapsulamento/Produtos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Encapsulamento
+{
+    class Produtos
+    {
+       
+
+        //Declaração de Váriaveis metodo GET - SET 
+        string _nome;
+        double _preco;
+        int _quantidade; 
+
+     
+
+        //Construtor 
+        public Produtos(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome() //usa o return para passar as informaçoes
+        {
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)//evitar o nulo
+            {
+                _nome = nome;
+            }
+
+        }
+       
+        
+        public double Preco
+        {
+            get { return _preco; }
+        }
+
+        public int Qtde
+        {
+            get { return _quantidade; }
+        }
+        /*
+          -- Fim -- 
+         */
+
+        public double ValorTotalEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionaProduto(int qtde)
+        {
+            _quantidade += qtde;
+        }
+
+        public void RemoveProduto(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", R$ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pega os dados que o usuario digitar
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.WriteLine("Nome");
+            string nome = Console.ReadLine();
+            
+            Console.WriteLine("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Quantidade: ");
+            int qtde = int.Parse(Console.ReadLine());
+            Produtos produto = new Produtos(nome, preco, qtde);
+
+            //mostra as informaçoes na tela
+            Console.WriteLine("Dados do produto: " + produto );
+
+            //add os produtos de acordo com a quantidade que o usario digitar
+            Console.WriteLine("Digite o número de produtos a serem adicionados: ");
+            int _qtde = int.Parse(Console.ReadLine());
+            produto.AdicionaProduto(_qtde);
+            Console.WriteLine("Produto atualizado: " + produto);
+
+            //remove os produtos de acordo com a quantidade que o usuario digitar 
+            Console.WriteLine("Informea quantidade de produtos a serem removidos: ");
+            _qtde = int.Parse(Console.ReadLine());
+            produto.RemoveProduto(_qtde);
+            Console.WriteLine("Produtos atualizados: " + produto);
+
+        }
+    }
+    }
+
